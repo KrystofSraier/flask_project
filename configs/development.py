9a14f69a7be0ca7080d463bfd8ae93f07d8e5e28
KrystofSraier <krystofsraier17@gmail.com>
2021-02-26 07:01:14 +0000

    heroku stuff

--- configs/development.py
+++ configs/development.py
@@ -1,5 +1,14 @@
+import os
+
+SECRET_KEY = b'\x8eJ|P7\x8c\xe6X\xb3\x9c\xaf\x17C\xbaz\x17\xbb\xc81`_\xe3\xac\xc2'
+USERNAME = "admin"
+PASSWORD = "admin"
+
 DEBUG = True
 # DATABASE = "/vagrant/blog.db"
 SQLALCHEMY_DATABASE_URI = "sqlite:////vagrant/blog.db"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 CELERY_BROKER_URL="pyamqp://guest@localhost//"
+
+EMAIL_USERNAME=os.environ.get("MDBLOG_EMAIL_USERNAME", None)
+EMAIL_PASSWORD=os.environ.get("MDBLOG_EMAIL_PASSWORD", None)
